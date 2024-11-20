6e90ada3f0c061a7e40ea14f5a14144a161c58a6
TysonPWilliams <tyson.williams95@gmail.com>
2024-11-20 20:47:18 +1100

    Finished lesson

---
+++ string_methods.py
@@ -0,0 +1,12 @@
+name = "John Smith"
+
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.split())
+
+name = "Hello there everyone!"
+print(name.split('e'))
+
+pieces = ['H', 'llo th', 'r', ' ', 'v', 'ryon', '!']
+print('e'.join(pieces))
